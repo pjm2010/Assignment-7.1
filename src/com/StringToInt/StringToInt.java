a3abf41dd9d2dfa879b027cd8b8836af466a40c8
pjm2010 <prithvijit20@gmail.com>
2017-07-10 21:08:00 +0530

    Add files via upload

---
+++ src/com/StringToInt/StringToInt.java
@@ -0,0 +1,15 @@
+package com.StringToInt;
+/*This class will describe the process of converting an int into a String*/
+public class StringToInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=10; ///Value of a int 
+		String str=String.valueOf(x); //User Class string to convert the int into String
+		System.out.println("The value of the string which got converted from the int is "+str); //Printing the value of the string
+		
+		
+	}
+
+}
